d3a76cbb4a5e6cdb6cd18a00a6bdd732d1b67e7e
Niharika2415-maker <naricityniharika@gmail.com>
2025-05-21 15:45:31 +0530

    Add files via upload

---
+++ countthenotes.py
@@ -0,0 +1,7 @@
+x= int(input("enter the amount to withdraw"))
+note_1= x//100
+note_2= (x%100)//50
+note_3= ((x%100)%50)//10
+print(note_1)
+print(note_2)
+print(note_3)
---
+++ percentagecalculation.py
@@ -0,0 +1,9 @@
+math= int(input("enter the marks"))
+science= int(input("enter the marks"))
+hindi= int(input("enter the marks"))
+english= int(input("enter the marks"))
+sum= math+science+hindi+english
+print(sum)
+perc= (sum/400)*100
+print(perc)
+ 
