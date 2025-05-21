58264b51db169397aff948b636c0909c20fe458f
Niharika2415-maker <naricityniharika@gmail.com>
2025-05-21 16:12:04 +0530

    Add files via upload

---
+++ congratulationsmessageacp.py
@@ -0,0 +1,7 @@
+text= str("congratulation")
+word= input("enter the word in lowercase:")
+uppercase_word= word.upper()
+print("uppercase_word:", uppercase_word)
+word= input("enter the word in uppercase:")
+lowercase_word= word.lower()
+print("lowercase_word:", lowercase_word)
