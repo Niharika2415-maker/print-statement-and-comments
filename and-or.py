2ec17cd5dcdc17ef989d1d6d708ad14f7cba5d8a
Niharika2415-maker <naricityniharika@gmail.com>
2025-05-28 15:39:45 +0530

    Add files via upload

---
+++ and-or.py
@@ -0,0 +1,20 @@
+x= 24
+y= 11
+z= 35
+if x and y and z:
+    print("all the numbers have boolean value as true")
+else:
+    print("at least one number has boolean value as false")
+
+o= 8
+p= 20
+q= 22
+if o>0 or p>0:
+    print ("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
+
+if p>0 or q>0:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
---
+++ notoperator.py
@@ -0,0 +1,17 @@
+n= 11
+t= 42
+s= 57
+print(n!=t)
+print(t!=s)
+x= "hello"
+y= "write"
+if x!=y:
+    print(x, 'and',y, 'are different')
+a= 22
+b= 33
+if (a==22)!= (b==33):
+    print("hello")
+text= int(input("enter a number"))
+if text/2!=0:
+    print(text,"is not an even number")
+       
