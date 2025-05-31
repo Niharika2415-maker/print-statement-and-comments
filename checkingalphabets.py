4c100057f65f515170cdb3d28444430ecb535bfa
Niharika2415-maker <naricityniharika@gmail.com>
2025-05-31 19:34:59 +0530

    Add files via upload

---
+++ checkingalphabets.py
@@ -0,0 +1,5 @@
+x= ("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z")
+if (x=="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,qr,s,t,u,v,w,x,y,z"):
+    print("it is a character")
+else:
+    print ("it is not a character")
