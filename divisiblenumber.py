f46fb4dcb0dceaa193d746f7ecb7cf1614ea2ed5
Niharika2415-maker <naricityniharika@gmail.com>
2025-06-04 16:01:31 +0530

    Add files via upload

---
+++ divisiblenumber.py
@@ -0,0 +1,8 @@
+print("enter a numerator")
+a= int(input())
+print("enter a denominator")
+b= int(input())
+if a%b==0:
+    print("it is divisible")
+else:
+    print("it is not divisible")
---
+++ operatorprecedence.py
@@ -0,0 +1,13 @@
+a= 11
+b= 24
+c= 22
+d= 20
+e= 0
+e=(a+b)*c/d;
+print(e)
+name= "nithvika"
+age= 0
+if name=="nithvika"or name=="niharika"and age>=20:
+    print("welcome")
+else:
+    print("goodbye")
