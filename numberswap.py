a03fdd89f9f0f2dcdf1e2baccb3c2f6f9149c5ec
Niharika2415-maker <naricityniharika@gmail.com>
2025-06-08 17:01:03 +0530

    Add files via upload

---
+++ numberswap.py
@@ -0,0 +1,8 @@
+a= int(input("enter the first number:"))
+b= int(input("enter the second number:"))
+temp=a
+a=b
+b=temp
+print("after swapping:")
+print("first number:",a)
+print("second number:",b)
