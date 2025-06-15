7896745e0621c5b3ecc0ada9d735035b73024e5e
Niharika2415-maker <naricityniharika@gmail.com>
2025-06-15 08:31:57 +0530

    Add files via upload

---
+++ reverseastring.py
@@ -0,0 +1,6 @@
+string= input("enter a word")
+string1=('')
+for i in string:
+    string1=i+string1
+print("the original string is",string)
+print("the reversed string is",string1)
---
+++ sumofwholenumbers.py
@@ -0,0 +1,6 @@
+print("this calculates the sum of whole numbers")
+num1= int(input("enter any number"))
+sum= 0
+for i in range (1,num1+1):
+ sum= sum+i
+ print("the sum of whole number is=",sum)
