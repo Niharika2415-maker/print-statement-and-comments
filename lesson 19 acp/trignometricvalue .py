5d419e321183f60c752e92c51e3fed287157e217
Niharika2415-maker <naricityniharika@gmail.com>
2025-08-03 16:17:21 +0530

    Add files via upload

---
+++ lesson 19 acp/trignometricvalue .py
@@ -0,0 +1,8 @@
+import math
+
+angle = float(input("Enter angle in degrees: "))
+radian = math.radians(angle)
+
+print("sin =", math.sin(radian))
+print("cos =", math.cos(radian))
+print("tan =", math.tan(radian))
