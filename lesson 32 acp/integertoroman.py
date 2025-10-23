ae7e1579b1393d4aab17cba82df4eea33de695b8
Niharika2415-maker <naricityniharika@gmail.com>
2025-10-23 21:56:44 +0530

    Add files via upload

---
+++ lesson 32 acp/integertoroman.py
@@ -0,0 +1,15 @@
+class IntegerToRoman:
+    def to_roman(self, num):
+        val = [1000, 900, 500, 400,100, 90, 50, 40,10, 9, 5, 4,1]
+        syb = ["M", "CM", "D", "CD","C", "XC", "L", "XL", "X", "IX", "V", "IV","I"]
+        roman_num = ""
+        i = 0
+        while num > 0:
+            for _ in range(num // val[i]):
+                roman_num += syb[i]
+                num -= val[i]
+            i += 1
+        return roman_num
+converter = IntegerToRoman()
+number = int(input("Enter an integer: "))
+print(f"The Roman numeral equivalent of {number} is: {converter.to_roman(number)}")
